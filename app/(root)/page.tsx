5d876e2b9f1a202e1fb2885c66cf3562265298ca
sebastian__gabriel <espositosebastian@hotmail.com>
2024-01-15 15:46:55 -0300

    First commit c

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
+  
   return (
     <>
       <section className='bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10'>
--- lib/database/index.ts
+++ lib/database/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI;
+export const MONGODB_URI = process.env.MONGODB_URI;
+
+
 
 let cached = (global as any).mongoose || { conn: null, promise: null };
 
@@ -19,46 +21,7 @@ export const connectToDatabase = async () => {
   return cached.conn;
 }
 
-// export const connectToDatabase = async () => {
-//     try {
-//       if (cached.conn) return cached.conn;
-  
-//       if (!MONGODB_URI) throw new Error('MONGODB_URI is missing');
-  
-//       cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
-//         dbName: 'evently',
-//         bufferCommands: false,
-//       });
-  
-//       cached.conn = await cached.promise;
-  
-//       // Realiza una operación simple para probar la conexión (puedes ajustar esto según tu modelo)
-//       const testQueryResult = await cached.conn.collection('tu_coleccion').findOne({});
-  
-//       console.log('Conexión a la base de datos establecida correctamente.');
-//       console.log('Resultado de la prueba:', testQueryResult);
-  
-//       return cached.conn;
-//     } catch (error) {
-//       console.error('Error al intentar conectar a la base de datos:', error);
-//       return null;
-//     }
-//   }
 
-// mongoose.set("strictQuery", true);
-// const connectToDatabase = async () => {
-//     return new Promise<void>((resolve, reject) => {
-//       mongoose.connect(MONGODB_URI as string, (error) => {
-//         if (error) {
-//           console.error('Error al conectar a la base de datos:', error);
-//           reject(error);
-//         } else {
-//           console.log('Conectado a la base de datos');
-//           resolve();
-//         }
-//       });
-//     });
-//   };
 
 
 // mongodb+srv://ecommerce-Coder:46583954@coderdatabase.bwdyham.mongodb.net/?retryWrites=true&w=majority
